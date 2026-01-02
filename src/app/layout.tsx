88993e8ed62f47199d288ad87dacbb3ed396abdd
Josuecandia <121037855@upq.edu.mx>
2026-01-02 00:08:40 -0600

    imagen comprimida

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/og-image.jpg", // <--- LEER PUNTO 2 ABAJO
+        url: "/og-image.jpg", 
         width: 1200,
         height: 630,
         alt: "JOZUL Agencia Digital",
